d81248e4aa259cae5791b20edd9bcdf475ae59ce
tqphan <quoc@hush.ai>
2016-04-08 13:57:33 -0700

    added models basic structures

---
+++ OnscreenKeyboard.Models/Models/KeyBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnscreenKeyboard.Model
+{
+    public class KeyBase
+    {
+        protected double x;
+        protected double y;
+        protected double width;
+        protected double height;
+    }
+}
