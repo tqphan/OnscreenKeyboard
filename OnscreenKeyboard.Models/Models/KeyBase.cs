ab1755e01818ccfcf0974484cf80c3db39060888
tqphan <quoc@hush.ai>
2016-04-08 17:24:43 -0700

    implement properties

--- OnscreenKeyboard.Models/Models/KeyBase.cs
+++ OnscreenKeyboard.Models/Models/KeyBase.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace OnscreenKeyboard.Model
 {
-    public class KeyBase
+    public abstract class KeyBase: BindableBase
     {
-        protected double x;
-        protected double y;
-        protected double width;
-        protected double height;
+        private double x;
+        private double y;
+        private double width;
+        private double height;
+
+        public double X
+        {
+            get { return this.x; }
+            set { SetProperty(ref this.x, value); }
+        }
+
+        public double Y
+        {
+            get { return this.y; }
+            set { SetProperty(ref this.y, value); }
+        }
+
+        public double Width
+        {
+            get { return this.width; }
+            set { SetProperty(ref this.width, value); }
+        }
+
+        public double Height
+        {
+            get { return this.height; }
+            set { SetProperty(ref this.height, value); }
+        }
     }
 }
