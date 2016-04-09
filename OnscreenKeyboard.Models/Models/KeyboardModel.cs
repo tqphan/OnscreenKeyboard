370f73fc12281b08dba7075c6fc04468fe43ed81
tqphan <quoc@hush.ai>
2016-04-08 18:32:39 -0700

    added panel collection

--- OnscreenKeyboard.Models/Models/KeyboardModel.cs
+++ OnscreenKeyboard.Models/Models/KeyboardModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
+using System.Collections.ObjectModel;
 
 namespace OnscreenKeyboard.Model
 {
-    public class KeyboardModel
+    public class KeyboardModel : BindableBase
     {
+        private ObservableCollection<KeysPanel> panels;
+
+        public ObservableCollection<KeysPanel> Panels
+        {
+            get { return this.panels; }
+            set
+            {
+                SetProperty(ref this.panels, value);
+            }
+        }
     }
 }
