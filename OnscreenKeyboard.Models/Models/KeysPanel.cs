75bc4ef0c2f38a3964678e95c90418b401c8400c
tqphan <quoc@hush.ai>
2016-04-08 18:04:49 -0700

    added keys to panel

--- OnscreenKeyboard.Models/Models/KeysPanel.cs
+++ OnscreenKeyboard.Models/Models/KeysPanel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace OnscreenKeyboard.Model
 {
-    class KeysPanel
+    public class KeysPanel : BindableBase
     {
+        private ObservableCollection<KeyBase> keys;
+
+        public ObservableCollection<KeyBase> Keys
+        {
+            get { return this.keys; }
+            set
+            {
+                SetProperty(ref this.keys, value);
+            }
+        }
     }
 }
